112063d337b06a596359377d2a9bdf710998aa24
Augusto Marques <augusto.marques12@gmail.com>
2021-03-31 22:41:41 -0300

    Iniciando o projeto

---
+++ src/main/java/br/com/amarques/smartcookbook/SmartCookbookApplication.java
@@ -0,0 +1,13 @@
+package br.com.amarques.smartcookbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCookbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCookbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/amarques/smartcookbook/SmartCookbookApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.amarques.smartcookbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCookbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
