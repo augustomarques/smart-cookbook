db0e325beb5cec832202c6dcce46851dd50921ea
Augusto Marques <augusto.marques@mercadolivre.com>
2021-04-03 11:23:16 -0300

    Adicionando suporte ao Flyway

---
+++ src/main/resources/db/migration/V1__initialize_database.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `receitas` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `modo_preparo` varchar(255) DEFAULT NULL,
+  `nome` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `ingredientes` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) DEFAULT NULL,
+  `receita_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK__ingrediente__receita_id` (`receita_id`),
+  CONSTRAINT `FK__ingrediente__receita_id` FOREIGN KEY (`receita_id`) REFERENCES `receitas` (`id`)
+);
